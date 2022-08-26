91b959cd97e055ae49bd27c2538d52bc717b33d4
Rodrigo Almeida <rodrigo.ddeus.almeida@gmail.com>
2022-08-26 09:02:47 -0300

    Adding path parameter to Api GET Method and fixing getUser Lambda

--- infra/api.tf
+++ infra/api.tf
@@ -3,27 +3,55 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
   description = "API from Celer application"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+##PROXYS
+resource "aws_api_gateway_resource" "api_proxy_user" {
   rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.api_gateway.root_resource_id}"
   path_part   = "user"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api_gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "POST"
-  authorization = "NONE"
-
+resource "aws_api_gateway_resource" "api_proxy_user_id" {
+  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
+  parent_id   = "${aws_api_gateway_resource.api_proxy_user.id}"
+  path_part   = "{id}"
 }
 
-resource "aws_api_gateway_integration" "integration_create_user" {
-  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+#METHODS
 
+resource "aws_api_gateway_method" "api_method_user" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api_gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.api_proxy_user.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "api_method_user_id" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api_gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.api_proxy_user_id.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+
+#INTEGRATIONS
+
+resource "aws_api_gateway_integration" "api_integration_create_user" {
+  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
+  resource_id = "${aws_api_gateway_method.api_method_user.resource_id}"
+  http_method = "${aws_api_gateway_method.api_method_user.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.lambda_create_user.invoke_arn}"
 }
+
+resource "aws_api_gateway_integration" "api_integration_get_user" {
+  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
+  resource_id = "${aws_api_gateway_method.api_method_user_id.resource_id}"
+  http_method = "${aws_api_gateway_method.api_method_user_id.http_method}"
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.lambda_get_user.invoke_arn}"
+}
+
+
 
--- src/lambda/getUser.py
+++ src/lambda/getUser.py
@@ -1,16 +1,17 @@
 import json
+import jsonpickle
 from UserRepository import get
 
-def lambda_handler(event,context):
-    #body = json.loads(event.get('body'))
-    id = event.get('id')
-    item = get(id)
 
+def lambda_handler(event, context):
+    id = event.get('pathParameters').get('id')
+    item = get(id)
+    
     statusCode = 200
-    if(item is None):
+    if (item is None):
         statusCode = 404
 
     return {
     'statusCode': statusCode, 
-    'body': item
+    'body': jsonpickle.encode(item, unpicklable=False)
     }
