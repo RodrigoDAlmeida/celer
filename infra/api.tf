4d8edb1980d76fd4fe7e4b6b6daaa530f0a04a55
Rodrigo Almeida <rodrigo.ddeus.almeida@gmail.com>
2022-08-29 09:35:53 -0300

    Fixing Get method response type

--- infra/api.tf
+++ infra/api.tf
@@ -54,26 +54,26 @@ resource "aws_api_gateway_integration" "api_integration_get_user" {
   uri                     = "${aws_lambda_function.lambda_get_user.invoke_arn}"
 }
 
-#resource "aws_api_gateway_method_response" "api_integration_get_user_response" {
-#  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
-#  resource_id = "${aws_api_gateway_resource.api_proxy_user_id.id}"
-#  http_method = "${aws_api_gateway_method.api_method_user_id.http_method}"
-#  status_code = "200"
-#   response_models = {
-#         "application/json" = "Empty"
-#    }
-#}
+resource "aws_api_gateway_method_response" "api_integration_get_user_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
+  resource_id = "${aws_api_gateway_resource.api_proxy_user_id.id}"
+  http_method = "${aws_api_gateway_method.api_method_user_id.http_method}"
+  status_code = "200"
+   response_models = {
+         "application/json" = "Empty"
+    }
+}
 
-#resource "aws_api_gateway_integration_response" "api_get_user_integration_response" {
-#  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
-#  resource_id = "${aws_api_gateway_resource.api_proxy_user_id.id}"
-#  http_method = "${aws_api_gateway_method.api_method_user_id.http_method}"
-#  status_code = "${aws_api_gateway_method_response.api_integration_get_user_response.status_code}"
-#
-#   response_templates = {
-#       "application/json" = ""
-#   } 
-#}
+resource "aws_api_gateway_integration_response" "api_get_user_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
+  resource_id = "${aws_api_gateway_resource.api_proxy_user_id.id}"
+  http_method = "${aws_api_gateway_method.api_method_user_id.http_method}"
+  status_code = "${aws_api_gateway_method_response.api_integration_get_user_response.status_code}"
+
+   response_templates = {
+       "application/json" = ""
+   } 
+}
 
 
 
