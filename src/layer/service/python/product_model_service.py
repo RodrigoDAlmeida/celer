dc41b4be691ed012e5d4a7e2a9f6e4e291afc2be
Rodrigo Almeida <rodrigo.ddeus.almeida@gmail.com>
2022-09-20 08:20:58 -0300

    creating product-model context in service lambda layer

---
+++ src/layer/service/python/product_model_service.py
@@ -0,0 +1,52 @@
+from repository import product_model_repository
+from repository import company_repository
+from model.ProductModel import ProductModel
+
+
+def create(name, product_id, purchase_price, sale_price):
+    check_product_id(product_id)
+    new_product_model = ProductModel(name, product_id, purchase_price, sale_price)
+    while product_model_repository.get(new_product_model.id):
+        new_product_model = ProductModel(name, product_id, purchase_price, sale_price)
+    product_model_repository.put_item(new_product_model)
+    return new_product_model
+
+
+def get_by_id(product_model_id):
+    product_model = product_model_repository.get(product_model_id)
+    if not product_model:
+        raise Exception("product model {} not found".format(product_model_id))
+    return product_model
+
+
+def get_by_product_id(product_id):
+    return product_model_repository.get_by_product_id(product_id)
+
+
+def get_all():
+    return product_model_repository.scan()
+
+
+def remove(product_model_id):
+    check_exists(product_model_id)
+    response = product_model_repository.delete_item(product_model_id)
+    return response.get('ResponseMetadata').get('HTTPStatusCode') == 200
+
+
+def update(name, product_id, purchase_price, sale_price, product_model_id):
+    check_exists(product_model_id)
+    check_product_id(product_id)
+    product_model = ProductModel(name, product_id,  purchase_price, sale_price, product_model_id)
+    response = product_model_repository.put_item(product_model)
+    return product_model if response.get('ResponseMetadata').get('HTTPStatusCode') == 200 else response
+
+
+def check_exists(product_model_id):
+    if not get_by_id(product_model_id):
+        raise Exception('product model {} not found'.format(product_model_id))
+
+
+def check_product_id(product_id):
+    product = company_repository.get(product_id)
+    if not product:
+        raise Exception("product {} not found".format(product_id))
---
+++ src/layer/service/python/repository/product_model_repository.py
@@ -0,0 +1,44 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('celer-product')
+
+
+def put_item(product_model):
+    response = table.put_item(
+        Item={
+            'id': product_model.id,
+            'name': product_model.name,
+            'product_id': product_model.product_id,
+            'purchase_price': product_model.purchase_price,
+            'sale_price': product_model.sale_price
+        }
+    )
+    return response
+
+
+def get(id):
+    response = table.get_item(Key={
+        'id': id
+    })
+    return response.get('Item')
+
+
+def scan():
+    return table.scan().get('Items')
+
+
+def delete_item(id):
+    response = table.delete_item(
+        Key={
+            'id': id
+        })
+    return response
+
+
+def get_by_product_id(product_id):
+    return table.query(IndexName='product-id-index', KeyConditionExpression=Key('product_id').eq(product_id)).get("Items")
+
+
+
